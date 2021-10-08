7f6b3ca75a96b5accd9748379583fc27e2c79e86
Martin de Bes on MacBook Air <martin.de.bes@me.com>
2021-10-08 14:19:30 +0200

    CSS op orde en alles in App.js gezet. Later weer moduleren

--- my-groceries-app/src/components/shoppingCart.js
+++
@@ -1,3 +0,0 @@
-const shoppingCart = [{ id: 6, title: "Peren", list: "s" }];
-
-export default shoppingCart;
---
+++ my-groceries-app/src/shoppingCart.js
@@ -0,0 +1,6 @@
+const shoppingCart = [
+  { id: 6, title: "Peren", list: "s" },
+  { id: 1392390, title: "Augurken", list: "s" }
+];
+
+export default shoppingCart;
